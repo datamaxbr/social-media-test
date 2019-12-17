e0fc951ecdb675c301510bce11fcd93ec235d8f3
Dan Leclaire <datamaxbr@gmail.com>
2019-12-17 08:25:20 -0500

    Completed the SmartContract

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -10,16 +10,22 @@ contract SocialNetwork {
     uint id;
     string content;
     uint tipAmount;
-    address author;
+    address payable author;
   }
 
   event PostCreated(
     uint id,
     string content,
     uint tipAmount,
-    address author    
+    address payable author    
     );
 
+  event PostTipped(
+    uint id,
+    string content,
+    uint tipAmount,
+    address payable author    
+    );
   constructor() public {
     name = "Worldwide Dapp Lottery";
   }
@@ -37,4 +43,22 @@ contract SocialNetwork {
     //Trigger Event
     emit PostCreated(postCount, _content, 0, msg.sender);
   }
+
+  function tipPost(uint _id) public payable {
+    // Make sure the id is valid
+    require(_id > 0 && _id <= postCount);
+    // fetch the post
+    Post memory _post = posts[_id];
+    // fetch the author
+    address payable _author = _post.author;
+    // Pay the author by sending the Ether
+    address(_author).transfer(msg.value);
+    // increment the tip amount
+    _post.tipAmount = _post.tipAmount + msg.value;
+    // Update the post
+    posts[_id] = _post;
+    // Trigger an update
+    emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
+  }
+  
 }
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -31,10 +31,13 @@ require('chai')
     describe('Posting: ', async () => {
       let result, postCount
 
-      it ('Post creation enabled', async () => {
+      before(async () => {
         result = await socialNetwork.createPost('This is my first post', { from: author })
         postCount = await socialNetwork.postCount()
+      })
 
+      it ('Post creation enabled', async () => {
+        
         //Success
         assert.equal(postCount, 1)
         const event = result.logs[0].args
@@ -47,11 +50,44 @@ require('chai')
         await socialNetwork.createPost('', { from: author }).should.be.rejected;
 
       })
-      // it ('Post listing feature is enabled', async () => {
-      //   //TODO Fill me in
-      // })
-      // it ('Author tipping is allowed', async () => {
-      //   //TODO Fill me in
-      // })
+      it ('Post listing feature is enabled', async () => {
+        const post = await socialNetwork.posts(postCount)
+        assert.equal(post.id.toNumber(), postCount.toNumber(), 'The Id is correct')
+        assert.equal(post.content, 'This is my first post', 'The content is correct')
+        assert.equal(post.tipAmount, '0', 'The tip amount is correct')
+        assert.equal(post.author, author, 'The author is correct')
+      })
+      it ('Author tipping is allowed', async () => {
+        // Track the author balance before purchase
+        let oldAuthorBalance
+        oldAuthorBalance = await web3.eth.getBalance(author)
+        oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+       
+        result = await socialNetwork.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+        //Success
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), postCount.toNumber(), 'The Id is correct')
+        assert.equal(event.content, 'This is my first post', 'The content is correct')
+        assert.equal(event.tipAmount, '1000000000000000000', 'The tip amount is correct')
+        assert.equal(event.author, author, 'The author is correct')
+
+        // Check that author received funds
+        let newAuthorBalance
+        newAuthorBalance = await web3.eth.getBalance(author)
+        newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+        let tipAmount
+        tipAmount = web3.utils.toWei('1', 'Ether')
+        tipAmount = new web3.utils.BN(tipAmount)
+
+        const exepectedBalance = oldAuthorBalance.add(tipAmount)
+
+        assert.equal(newAuthorBalance.toString(), exepectedBalance.toString())
+
+        // FAILURE: Tries to tip a post that does not exist
+        await socialNetwork.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+
+      })
     })
   })
