f1dbb4e2858edb3180acb2d1c88b19faabd69ccf
Dan Leclaire <datamaxbr@gmail.com>
2019-12-18 06:17:21 -0500

    Matamask connection OK

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,76 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You may want to consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load the account
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
+              <div className="content mr-auto ml-auto"> <br/>
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://cyberassistance.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <br/><br/>
+                <h1>Decentralized Social Network</h1>
+                <h1>ETH DAPP Demonstration</h1>
                 <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+                  Using Smart Contracts with Ether on the Ethereum Network 
+                </p>
+                <p>
+                  Allowing any authors to post new content <br/>
+                  and allowing any readers to tip the authors.  
                 </p>
                 <a
                   className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://cyberassistance.com"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  BLOCKCHAIN WORKBENCH AREA
                 </a>
               </div>
             </main>
